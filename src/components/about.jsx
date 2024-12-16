e8b955c6d212e1cc760e0e71f761fb0df694c7ed
ZhenYu <zhenn.whoop@gmail.com>
2024-12-16 08:39:14 +0100

    chore(tooltisch): tooltisch

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export const About = (props) => {
+  console.log(props.data);
   return (
     <div id="about">
       <div className="container">
@@ -13,23 +14,29 @@ export const About = (props) => {
             <div className="about-text">
               <h2>Über uns</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              {/*<p dangerouslySetInnerHTML={{__html: props.data ? props.data.paragraph : "loading..."}}></p>
+              <p>
+                {props.data ? props.data.paragraph.split("\n") : "loading..."}
+                Eigene Methode machen, um die stellen mit \n zu splitten und an diesen stellen ein <br> einbauen
+              </p>
+              */}
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
+                        ? props.data.Why.map((d, i) => (
+                            <li key={`${d}-${i}`}>{d}</li>
                         ))
-                      : "loading"}
+                        : "loading"}
                   </ul>
                 </div>
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
+                        ? props.data.Why2.map((d, i) => (
+                            <li key={`${d}-${i}`}> {d}</li>
                         ))
-                      : "loading"}
+                        : "loading"}
                   </ul>
                 </div>
               </div>
